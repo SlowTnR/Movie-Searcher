474e49005cce1e7ace110ca2a9d366130eb4ac02
Ilja Patrushev <slowtnr@gmail.com>
2020-10-06 14:43:34 +0300

    Add UI

--- Movie Searcher/ViewController.swift
+++ Movie Searcher/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// UI
+// Network request
+// Tap a cell to see info
+// Custom cell
+
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var field: UITextField!
+    
+    var movies = [Movie]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
+        
+    }
+    
+    // MARK: FIELD
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchMovies()
+        return true
+    }
+    
+    func searchMovies(){
+        field.resignFirstResponder()
+    }
+    
+    //MARK: TABLE
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // show movies details
     }
 
 
 }
 
+struct Movie {
+    
+}
